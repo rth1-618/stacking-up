5baa02cd0cd76691fff4ecb94f19390f83224af5
rth1-618 <rth1.618@gmail.com>
2022-07-17 20:37:36 +0530

    done

--- pr1/side.js
+++ pr1/side.js
@@ -1,4 +1,17 @@
-var ham = document.querySelector(".ham");
-ham.addEventListener("click", function () {
-  document.querySelector("body").classList.toggle("active");
-});
+// var ham = document.querySelector(".ham");
+// ham.addEventListener("click", function () {
+//   document.querySelector("body").classList.toggle("active");
+// });
+const openHandler = () =>
+  document.querySelector(".sidebar").classList.remove("hidden");
+const closeHandler = () => {
+  document.querySelector(".sidebar").classList.add("close_sidebar");
+  setTimeout(() => {
+    document.querySelector(".sidebar").classList.add("hidden");
+    document.querySelector(".sidebar").classList.remove("close_sidebar");
+  }, 400);
+};
+document
+  .getElementById("open_sidebar_button")
+  .addEventListener("click", openHandler);
+document.querySelector(".close_button").addEventListener("click", closeHandler);
